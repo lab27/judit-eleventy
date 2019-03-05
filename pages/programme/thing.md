b8431a873a20e95af952e4c286ad8e4946fb4624
Nick Niles <nickyniles@gmail.com>
2019-03-05 14:00:01 +0100

    [feat] add swup and clean up

--- pages/programme/thing.md
+++
@@ -1,8 +0,0 @@
----
-title: thing
-date: 2019-02-26T16:15:20.144Z
-permalink: /programme/thing.html
-navtitle: Thing
-tags: nav
----
-I'm . THINGGGG
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+import Swup from 'swup';
+console.log('swup', Swup);
+console.log('thing!');
+
+const currentYear = new Date().getFullYear()
+console.log(currentYear);
