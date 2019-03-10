fe5c23393292834399c4abc3e403bb9ccc938302
Nick Niles <nickyniles@gmail.com>
2019-03-10 14:42:11 +0100

    [featat] add some swup

--- _includes/assets/js/app.js
+++ _includes/assets/js/app.js
@@ -1,6 +1,9 @@
-import Swup from 'swup';
 console.log('swup', Swup);
 console.log('thing!');
 
 const currentYear = new Date().getFullYear()
 console.log(currentYear);
+
+swup.on('pageView', function (event) {
+    console.log('page View', event)
+});
